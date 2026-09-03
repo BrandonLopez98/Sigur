eb56f16fbf1fae9c8b1cda01a3cc394a50b8c12d
Brandon Lopez <118473149+BrandonLopez98@users.noreply.github.com>
2026-09-02 23:07:07 -0500

    Update postQuery.js

--- src/controllers/Query/postQuery.js
+++ src/controllers/Query/postQuery.js
@@ -10,7 +10,7 @@ module.exports = async ({ user_id, document_type, document_number, search_name, 
     // 2. Verificar que el usuario exista en la base de datos
     const userExists = await User.findByPk(user_id);
     if (!userExists) {
-      throw new Error(`El usuario con el ID ${user_id} no existe en la base de datoss.`);
+      throw new Error(`El usuario con el ID ${user_id} no existe en la base de datos.`);
     }
 
     // 3. Crear la query vinculada al usuario utilizando los campos definidos en tu modelo
