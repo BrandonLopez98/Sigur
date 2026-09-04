2a6d293d05829d3f27264cc062c5705d5702c35e
Brandon Lopez <118473149+BrandonLopez98@users.noreply.github.com>
2026-09-04 11:54:44 -0500

    Router getQuerys

--- src/routes/QueryRoutes.js
+++ src/routes/QueryRoutes.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+
 const postQuery = require('../controllers/Query/postQuery');
+const getQuerys = require('../controllers/Query/getQuerys');
+
+router.get('/', async (req, res) => {
+  try {
+    const { id } = req.query;
+    
+    const resultado = await getQuerys(id);
+    return res.status(200).json(resultado);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
