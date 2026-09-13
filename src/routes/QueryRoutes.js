ffc4a06fa7a3491e4b374f344901003ee4214e0b
Tu Nombre <brandonlopezmonroy@gmail.com>
2026-09-12 22:59:56 -0500

    get por ai de user

--- src/routes/QueryRoutes.js
+++ src/routes/QueryRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const postQuery = require('../controllers/Query/postQuery');
-const getQuerys = require('../controllers/Query/getQuerys');
+const getQuerys = require('../controllers/Query/GetQuerys');
 
 router.get('/', async (req, res) => {
   try {
-    const { id } = req.query;
+    const { user_id: userId, search } = req.query;
     
-    const resultado = await getQuerys(id);
+    const resultado = await getQuerys({ userId, search });
     return res.status(200).json(resultado);
   } catch (error) {
-    return res.status(404).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 });
 
