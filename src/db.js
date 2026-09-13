a275e9266d37301b27a8c73b1ce939348e60df6a
Tu Nombre <brandonlopezmonroy@gmail.com>
2026-09-12 20:04:16 -0500

    Vinculacion bases de datos

--- src/db.js
+++ src/db.js
@@ -31,10 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User } = sequelize.models;
+const { User, Query } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+// Un usuario puede tener muchas consultas (Querys)
+User.hasMany(Query, { foreignKey: 'user_id' });
+Query.belongsTo(User, { foreignKey: 'user_id' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
